52604c79e7759bb4c21cb6bc12a837fd3a1e48b0
Donatron <don.macarthur68@gmail.com>
2018-05-01 10:16:11 +1000

    dynamically creating rows and tiles
    css colour changes to tiles

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Declare variables
-var row = '<div class="row"></div>'
-var tile = '<div class="tile"></div>'
+var row = '<div class="row"></div>';
+var tile = '<div class="tile"></div>';
 var numbersThisGame = 0;
 var game;
 var heads = 0;
@@ -32,100 +32,114 @@ function countdown() {
 }
 
 // Check if game in progress before starting timer
-if (!gameInProgress) {
-  countdown();
+// if (!gameInProgress) {
+//   countdown();
+// }
+
+// Create Keno rows
+function createKenoGrid() {
+
+  // Add 4 rows to Heads Container
+  for (var i=1; i<=4; i++) {
+    $('.heads-container').append(row);
+  }
+
+  // Add 4 rows to Tails Container
+  for (var i=5; i<=8; i++) {
+    $('.tails-container').append(row);
+  }
+
+}
+createKenoGrid();
+
+function appendRowNumbers() {
+
+  // Append row number to row classes
+  var numberOfRows = $('.row').length;
+  console.log(numberOfRows);
+
+  for (var i=1; i<=numberOfRows; i++) {
+    var thisClass = 'row-' + i;
+    var thisRow = $('.row').eq(i-1)
+    thisRow.addClass(thisClass);
+  }
+
+}
+appendRowNumbers();
+
+// Add tiles to rows
+function addTiles() {
+
+  for (var i=1; i<=10; i++) {
+    $('.row').append(tile);
+  }
+
+}
+addTiles();
+
+// Declare variable for accessing individual tiles
+var numberOfTiles = $('.tile').length;
+
+function addTileData() {
+
+  // Append numbers to tiles
+  for (var i=0; i<=numberOfTiles; i++) {
+    var thisTile = $('.tile').eq(i);
+    thisTile.html(i + 1);
+
+    // Place overlays over all tiles
+    // thisTile.prepend('<div class="overlay"></div>');
+  }
+}
+addTileData();
+
+// Remove tile overlays from drawn numbers
+function removeTileOverlay(index) {
+  var thisTile = $('.tile').eq(index - 1);
+  thisTile.html(index);
 }
 
-// // Create Keno rows
-// function createKenoGrid() {
-//
-//   for (var i=1; i<=8; i++) {
-//     $('.numbers').append(row);
-//     $(row).addClass('row-' + i);
-//   }
-//
-// }
-// createKenoGrid();
-//
-// // Add tiles to rows
-// function addTiles() {
-//
-//   for (var i=1; i<=10; i++) {
-//     $('.row').append(tile);
-//   }
-//
-// }
-// addTiles();
-//
-// // Append row number to row classes
-// var numberOfRows = $('.numbers .row').length;
-//
-// for (var i=1; i<=numberOfRows; i++) {
-//   var thisClass = 'row-' + i;
-//   var thisRow = $('.numbers .row').eq(i-1)
-//   thisRow.addClass(thisClass);
-// }
-//
-// // Append numbers to tiles
-// var numberOfTiles = $('.row .tile').length;
-//
-// // Place overlays over all tiles
-// function addTileOverlays() {
-//   for (var i=0; i<=numberOfTiles; i++) {
-//     var thisTile = $('.tile').eq(i);
-//     thisTile.html(i + 1);
-//     thisTile.prepend('<div class="overlay"></div>');
-//   }
-// }
-// addTileOverlays();
-//
-// // Remove tile overlays from drawn numbers
-// function removeTileOverlay(index) {
-//   var thisTile = $('.tile').eq(index - 1);
-//   thisTile.html(index);
-// }
-//
-// // Create function for declaring CSS colours based on drawn number
-// function colorTiles(index) {
-//   switch(true) {
-//     case (index < 10):
-//       return '#BE2431'
-//       break;
-//     case (index>=10 && index<20):
-//       return '#0D6BB3'
-//       break;
-//     case (index>=20 && index<30):
-//       return '#04863C'
-//       break;
-//     case (index>=30 && index<40):
-//       return '#FAA330'
-//       break;
-//     case (index>=40 && index<50):
-//       return '#A93195'
-//       break;
-//     case (index>=50 && index<60):
-//       return '#F04F21'
-//       break;
-//     case (index>=60 && index<70):
-//       return '#90ACBD'
-//       break;
-//     case (index>=70 && index<numberOfTiles):
-//       return '#513397'
-//       break;
-//   }
-// }
-//
-// // Add background colour to tiles
-// function addTileColor() {
-//
-//   for (var i=0; i<numberOfTiles; i++) {
-//     var tileAtIndex = $('.tile').eq(i);
-//     tileAtIndex.css('background-color', colorTiles(i));
-//   }
-//
-// }
-// addTileColor();
-//
+// Create function for declaring CSS colours based on drawn number
+function colorTiles(index) {
+  switch(true) {
+    case (index < 10):
+      return 'linear-gradient(to right,#e6007e 0,#a81815 100%)'
+      break;
+    case (index>=10 && index<20):
+      return 'linear-gradient(to right,#009fe3 0,#522583 100%)'
+      break;
+    case (index>=20 && index<30):
+      return 'linear-gradient(to right,#f2e500 0,#007f2d 100%)'
+      break;
+    case (index>=30 && index<40):
+      return 'linear-gradient(to right,#ffed00 0,#e74011 100%)'
+      break;
+    case (index>=40 && index<50):
+      return 'linear-gradient(to right, rgba(169,49,149,1) 0%, rgba(121,8,103,1) 100%)'
+      break;
+    case (index>=50 && index<60):
+      return 'linear-gradient(to right, rgba(240,79,33,1) 0%, rgba(155,35,0,1) 100%)'
+      break;
+    case (index>=60 && index<70):
+      return 'linear-gradient(to right, rgba(144,172,189,1) 0%, rgba(59,102,127,1) 100%)'
+      break;
+    case (index>=70 && index<numberOfTiles):
+      return 'linear-gradient(to right, rgba(81,51,151,1) 0%, rgba(40,14,102,1) 100%)'
+      break;
+  }
+}
+
+// Add background colour to tiles
+function addTileColor() {
+
+  for (var i=0; i<numberOfTiles; i++) {
+    var tileAtIndex = $('.tile').eq(i);
+    tileAtIndex.css('background', colorTiles(i));
+  }
+
+}
+addTileColor();
+
 // // Create initial arrays of numbers 1 to 80 and drawn numbers
 // var numbersArray = [];
 // var drawnNumbers = [];
